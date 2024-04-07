4c2f5880edb07ac203d87eb1de0520361c5e9269
Jericho <jericho1338@gmail.com>
2024-04-07 16:11:01 +0200

    1st commit

---
+++ src/script/main.js
@@ -0,0 +1,45 @@
+(async () => {
+  const helpers = await import(`./helpers.js`);
+
+  if (shouldOpenNewWindow()) {
+    setInterval(() => {
+      const url = generateGoogleUrl();
+      window.open(url, "_blank");
+    }, 5000);
+  } else if (shouldClickGoogleResult()) {
+    clickGoogleResult();
+  } else {
+    // else close window
+    const closeTimer = setInterval(() => {
+      clearInterval(closeTimer);
+      window.close();
+    }, 4000);
+  }
+
+  function clickGoogleResult() {
+    var clickEvent = new MouseEvent("click", {
+      view: window,
+      bubbles: true,
+      cancelable: false,
+    });
+    var randomLink = document.getElementsByTagName("h3")[2];
+    randomLink.dispatchEvent(clickEvent);
+  }
+
+  function generateGoogleUrl() {
+    const words = helpers.wordList;
+    const word1 = words[Math.floor(Math.random() * words.length)];
+    const word2 = words[Math.floor(Math.random() * words.length)];
+    const term = `${word1} ${word2}`;
+    const googleUrl = `http://google.hr/search?q=${encodeURI(term)}`;
+    return googleUrl;
+  }
+
+  function shouldOpenNewWindow() {
+    return document.URL.indexOf("sibenik.in") > -1;
+  }
+
+  function shouldClickGoogleResult() {
+    return document.URL.indexOf(".google.") > -1;
+  }
+})();
